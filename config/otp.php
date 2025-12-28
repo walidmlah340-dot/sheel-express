c5f9e60e20d982d6e79f2aa6f107b59568352713
walidmlah340-dot <walidmlah340@gmail.com>
2025-12-29 02:32:13 +0300

    Update otp.php

--- config/otp.php
+++ config/otp.php
@@ -1,1 +1,10 @@
+<?php
+
+return [
+    'expire_minutes' => (int) env('OTP_EXPIRE_MINUTES', 5),
+
+    // للتجربة فقط (اعرض/اقبل كود ثابت)
+    'bypass' => filter_var(env('OTP_BYPASS', false), FILTER_VALIDATE_BOOL),
+    'bypass_code' => (string) env('OTP_BYPASS_CODE', '1234'),
+];
 

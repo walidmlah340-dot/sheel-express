67c9dd1e8a42567d20727e6e3b3b23cf6fecea8d
walidmlah340-dot <walidmlah340@gmail.com>
2025-12-29 02:35:25 +0300

    Update Shipment.php

--- app/Models/Shipment.php
+++ app/Models/Shipment.php
@@ -1,1 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Shipment extends Model
+{
+    protected $fillable = [
+        'customer_id',
+        'pickup_address',
+        'dropoff_address',
+        'package_desc',
+        'weight',
+        'cod_amount',
+        'status',
+    ];
+}
 

ea363dfe224d853368c5a38430226263ce6def8c
walidmlah340-dot <walidmlah340@gmail.com>
2025-12-29 02:34:43 +0300

    Update OtpCode.php

--- app/Models/OtpCode.php
+++ app/Models/OtpCode.php
@@ -1,1 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class OtpCode extends Model
+{
+    protected $fillable = [
+        'user_id', 'phone', 'code_hash', 'expires_at', 'used_at', 'attempts',
+    ];
+
+    protected $casts = [
+        'expires_at' => 'datetime',
+        'used_at' => 'datetime',
+    ];
+}
 
